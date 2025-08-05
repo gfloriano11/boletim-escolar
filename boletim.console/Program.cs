67bf462b0f7cf85823420efd3c79054e013bb537
gfloriano11 <gflorianodev@gmail.com>
2025-08-04 22:02:13 -0300

    feat: add source code

--- boletim.console/Program.cs
+++ boletim.console/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int quantity = 3;
+
+int age;
+string? name;
+
+// int ages = [];
+int[] ages = new int[quantity];
+string[] names = new string[quantity];
+
+for (int i = 0; i < quantity; i++)
+{
+    Console.Clear();
+    Console.Write($"Cadastrando Registro Nº {i + 1}");
+
+    Console.Write("Nome : ");
+    name = Console.ReadLine();
+
+    Console.Write("Nome : ");
+    age = int.Parse(Console.ReadLine() ?? "0");
+
+    Console.ReadKey();
+
+    names[i] = name ?? "No name";
+    ages[i] = age;
+}
+
+Console.Clear();
+Console.WriteLine("Students List");
+Console.WriteLine("-------------");
+
+for (int i = 0; i < quantity; i++)
+{
+    Console.WriteLine($"Showing Register Number {i + 1}:");
+    Console.WriteLine($"{names[i]} \t {ages[i]}");
+
+}
+
+Console.WriteLine("Press any button to continue");
+Console.ReadKey();
